0505e853960ce7521e9a9a72731a247be749f143
kady <abdelrahman.elkady@coligotech.com>
2019-07-15 12:44:19 +0200

    Add user selection on index.html

--- client/main.js
+++ client/main.js
@@ -1,3 +1,16 @@
+const userInput = document.querySelectorAll('input[name="user"]');
+const roomOneButton = document.querySelector('#room-1');
+const roomTwoButton = document.querySelector('#room-2');
+
+userInput.forEach(i => {
+  i.addEventListener('change', (ev) => {
+    const userId = ev.target.value;
+
+    roomOneButton.href = roomOneButton.href.replace(/userId=\d/, `userId=${userId}`);
+    roomTwoButton.href = roomOneButton.href.replace(/userId=\d/, `userId=${userId}`);
+  });
+});
+
 const socket = io('localhost:1337', {
   transportOptions: {
     polling: {
