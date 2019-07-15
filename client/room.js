4c0b40225948bba18fba21bc47b8cdd4640cfded
kady <abdelrahman.elkady@coligotech.com>
2019-07-15 14:59:58 +0200

    We did it :tada:

--- client/room.js
+++ client/room.js
@@ -1,62 +1,77 @@
+let socket;
 let data;
 
 let roomId;
 let userId;
 
-const tableBody = document.querySelector('#table-body');
+const tableBody = document.querySelector("#table-body");
+const currentlyConnectedSpan = document.querySelector("#currently-connected");
 
 const fetchData = () => {
-  return fetch('http://localhost:1337/data')
+  return fetch("http://localhost:1337/data")
     .then(res => res.json())
-    .then((responseBody) => {
+    .then(responseBody => {
       data = responseBody[`r${roomId}`];
     });
 };
 
 const populateTableWithData = () => {
-  const html = data.map(row => {
-    return `
+  const html = data
+    .map((row, i) => {
+      return `
       <tr>
         <td data-label="Name">${row.name}</td>
-        <td contenteditable="true" data-label="A1">${row.a1}</td>
-        <td contenteditable="true" data-label="A2">${row.a2}</td>
+        <td contenteditable="true" id="c-${i}0" data-cell data-label="A1">${ row.a1 }</td>
+        <td contenteditable="true" id="c-${i}1" data-cell data-label="A2">${ row.a2 }</td>
       </tr>
     `;
-  }).join('\n');
+    })
+    .join("\n");
 
   tableBody.innerHTML = html;
+
+  document.querySelectorAll("[data-cell]").forEach(c => {
+    c.addEventListener("blur", ev => {
+      const value = ev.target.innerText;
+      socket.emit("edit-cell", { cellId: c.id.substring(2), value });
+    });
+  });
 };
 
 const initSockets = () => {
-  const socket = io('localhost:1337', {
+  socket = io("localhost:1337", {
     transportOptions: {
       polling: {
         extraHeaders: {
-          'x-ws-secret': 'amxrZmRoamxmYXNkbGY',
-          'x-room-id': roomId,
-          'x-user-id': userId,
+          "x-ws-secret": "amxrZmRoamxmYXNkbGY",
+          "x-room-id": roomId,
+          "x-user-id": userId
         }
       }
     }
   });
 
-  socket.on('connect', () => {
+  socket.on("connect", () => {
     console.log(socket.id);
   });
 
-  socket.on('amr', (event) => {
-    console.log(event);
+  socket.on("connected-members-changed", n => {
+    currentlyConnectedSpan.innerHTML = n;
   });
 
-  socket.on('error', (err) => {
+  socket.on("error", err => {
     console.log(err);
+  });
+
+  socket.on("cell-updated", ({ cellId, value }) => {
+    document.querySelector(`#c-${cellId}`).innerHTML = value;
   });
 };
 
 const onLoad = () => {
   const params = new URLSearchParams(window.location.search);
-  roomId = params.get('roomId');
-  userId = params.get('userId');
+  roomId = params.get("roomId");
+  userId = params.get("userId");
 
   fetchData().then(() => {
     populateTableWithData();
@@ -64,4 +79,4 @@ const onLoad = () => {
   });
 };
 
-window.addEventListener('load', onLoad);
+window.addEventListener("load", onLoad);
--- server/sockets.js
+++ server/sockets.js
@@ -1,44 +1,72 @@
-const SocketIO = require('socket.io');
+const SocketIO = require("socket.io");
 
-const SUPER_DUPER_SECRET = 'amxrZmRoamxmYXNkbGY';
+const data = require("./db");
+
+const SUPER_DUPER_SECRET = "amxrZmRoamxmYXNkbGY";
 
 const users = [
-  { userId: "1", allowedRooms: ["1"] },
-  { userId: "2", allowedRooms: ["2"] },
+  { userId: "1", allowedRooms: ["1", "2"] },
+  { userId: "2", allowedRooms: ["2"] }
 ];
 
-const initSockets = (server) => {
+const initSockets = server => {
   const io = SocketIO(server);
 
   // Middleware
   io.use((socket, next) => {
-    const clientSecret = socket.handshake.headers['x-ws-secret'];
-    const userId = socket.handshake.headers['x-user-id'];
-    const requestedRoomId = socket.handshake.headers['x-room-id'];
+    socket.clientSecret = socket.handshake.headers["x-ws-secret"];
+    socket.userId = socket.handshake.headers["x-user-id"];
+    socket.requestedRoomId = socket.handshake.headers["x-room-id"];
 
-    if (clientSecret !== SUPER_DUPER_SECRET) { return next(new Error('secret authentication error')); }
+    if (socket.clientSecret !== SUPER_DUPER_SECRET) {
+      return next(new Error("secret authentication error"));
+    }
 
-    const user = users.find(u => u.userId === userId);
-    if (!user) { return next(new Error('authentication error')); }
+    const user = users.find(u => u.userId === socket.userId);
+    if (!user) {
+      return next(new Error("authentication error"));
+    }
 
-    if (!user.allowedRooms.includes(requestedRoomId)) {
-      return next(new Error('NOT AUTHORIZED'));
+    if (!user.allowedRooms.includes(socket.requestedRoomId)) {
+      return next(new Error("NOT AUTHORIZED"));
     }
 
     return next();
   });
 
-  io.on('connection', (socket) => {
-    console.log('Connection established with ', socket.id);
+  io.on("connection", socket => {
+    console.log("Connection established with ", socket.id);
 
-    socket.join('roomy');
+    socket.join(socket.requestedRoomId, () => {
+      const connectedUsers =
+        io.sockets.adapter.rooms[socket.requestedRoomId] &&
+        io.sockets.adapter.rooms[socket.requestedRoomId].length;
 
-    socket.on('update', () => {
-      console.log(socket.rooms);
+      io.to(socket.requestedRoomId).emit("connected-members-changed", connectedUsers);
+    });
+
+    socket.on("edit-cell", ({ cellId, value }) => {
+      const row = parseInt(`${cellId}`.charAt(0), 10);
+      const col = parseInt(`${cellId}`.charAt(1), 10);
+
+      // Sorry
+      const propName = `a${col + 1}`;
+
+      data[`r${socket.requestedRoomId}`][row][propName] = value;
+
+      io.to(socket.requestedRoomId).emit("cell-updated", { cellId, value });
+    });
+
+    socket.on("disconnect", () => {
+      const connectedUsers =
+        io.sockets.adapter.rooms[socket.requestedRoomId] &&
+        io.sockets.adapter.rooms[socket.requestedRoomId].length;
+
+      io.to(socket.requestedRoomId).emit("connected-members-changed", connectedUsers);
     });
   });
 
-  io.on('error', console.error);
+  io.on("error", console.error);
 };
 
 module.exports = initSockets;
