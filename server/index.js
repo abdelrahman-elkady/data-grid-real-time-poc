be98f88015bc8004f93fabec6e487b4480fb54ec
kady <abdelrahman.elkady@coligotech.com>
2019-07-15 15:25:59 +0200

    Modify PORT

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const http = require('http');
+
 const app = require('./app');
 const initSockets = require('./sockets');
+
+const PORT = process.env.PORT || 1337;
 
 const server = http.createServer(app);
 
 initSockets(server);
 
-server.listen(1337, () => { console.log('working on port 1337'); });
+server.listen(PORT, () => { console.log(`working on port ${PORT}`); });
 
 module.exports.server = server;
